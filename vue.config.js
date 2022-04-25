12d3710b343e25f2e149be9805d2d182860c2551
Vasilii Prianikov <88191516+vprianikov@users.noreply.github.com>
2022-04-25 23:16:04 +0300

    Added Pug JS

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.pug$/,
+          oneOf: [
+            {
+              resourceQuery: /^\?vue/,
+              use: ['pug-plain-loader'],
+            },
+            {
+              use: ['raw-loader', 'pug-plain-loader'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
